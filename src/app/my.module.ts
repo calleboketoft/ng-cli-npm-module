5a23379038131a252c4312f30732670b7635da57
Carl-Ola Boketoft <calleboketoft@gmail.com>
2017-07-13 16:32:48 +0200

    app -> my

--- src/app/my.module.ts
+++ src/app/my.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { MyComponent } from './my.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MyComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MyComponent]
 })
-export class AppModule { }
+export class MyModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { MyModule } from './app/my.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(MyModule);
