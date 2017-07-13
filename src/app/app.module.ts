52aa99dfc144230cda3a4a45c58bf9d67e30707f
Carl-Ola Boketoft <calleboketoft@gmail.com>
2017-07-13 21:27:05 +0200

    now it might work

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { MyModule } from './my/my.module'
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MyModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
